f21be1746c1de1e613f50ec9c9e89f32631a29b7
torhovenkov <torhovenkov@icloud.com>
2024-01-05 22:24:42 +0200

    custom shapes

---
+++ SwiftUIAdvanced/CustomShapes.swift
@@ -0,0 +1,61 @@
+//
+//  CustomShapes.swift
+//  SwiftUIAdvanced
+//
+//  Created by Vladyslav Torhovenkov on 05.01.2024.
+//
+
+import SwiftUI
+
+struct CustomShapes: View {
+    var body: some View {
+        ZStack {
+//            Triangle()
+//            Diamond()
+            Trapezoid()
+                .stroke(style: .init(lineWidth: 5, lineCap: .butt, dash: [10]))
+                .frame(width: 300, height: 100)
+        }
+    }
+}
+
+#Preview {
+    CustomShapes()
+}
+
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        }
+    }
+}
+
+struct Diamond: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            let horizontalOffset: CGFloat = rect.width * 0.2
+            path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX - horizontalOffset, y: rect.midY))
+            path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX + horizontalOffset, y: rect.midY))
+            path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        }
+    }
+}
+
+struct Trapezoid: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            let horizontalOffset: CGFloat = rect.width * 0.2
+            path.move(to: CGPoint(x: rect.minX + horizontalOffset, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX - horizontalOffset, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX + horizontalOffset, y: rect.minY))
+        }
+    }
+}
