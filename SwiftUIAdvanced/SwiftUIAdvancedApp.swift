0df9cb40a475fb3df7a842ed104cd7c89370eed8
torhovenkov <torhovenkov@icloud.com>
2024-01-05 03:31:36 +0200

    Initial Commit

---
+++ SwiftUIAdvanced/SwiftUIAdvancedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAdvancedApp.swift
+//  SwiftUIAdvanced
+//
+//  Created by Vladyslav Torhovenkov on 05.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAdvancedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
